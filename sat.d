e38d0efa7cf380521f4d0e42adf93b9172dd2977
Simon Bürger <simon.buerger@rwth-aachen.de>
2014-01-15 15:42:15 +0100

    strong literal type and deferred propagation (only in preprocessing)

--- sat.d
+++ sat.d
@@ -3,80 +3,157 @@ module sat;
 import std.stdio;
 import std.conv;
 import jive.array;
+import jive.queue;
 import jive.flatset;
+private import std.math : abs;
 private import std.algorithm : move, min, max;
+private import std.bitmanip : bitfields;
 import solver, clause;
 import std.parallelism;
 
+struct Lit
+{
+	union
+	{
+		uint toInt;
+		mixin(bitfields!(
+			bool, "sign", 1,
+			uint, "var", 31
+			));
+	}
+
+	alias toInt this;
+
+	this(uint var, bool sign)
+	{
+		this.var = var;
+		this.sign = sign;
+	}
+
+	int toDimacs() const @property
+	{
+		if(sign)
+			return -(var+1);
+		else
+			return var+1;
+	}
+
+	static Lit fromDimacs(int x)
+	{
+		Lit l;
+		l.sign = x<0;
+		l.var = abs(x)-1;
+		return l;
+	}
+
+	Lit neg() const @property
+	{
+		return Lit(var, !sign);
+	}
+}
+
+
 class Sat
 {
+	alias FlatSet!Lit Clause;
 	const string name;
-	Array!(FlatSet!int) clauses;	// length=0 means clause was removed
+	Array!Clause clauses;	// length=0 means clause was removed
 	Array!(Array!int) occs;	// can contain removed clauses
-	Array!bool assign;
+	Array!VarState var;
+	Queue!uint prop;	// propagation queue
 	int varCount;
+
+	enum : uint
+	{
+		undef = 0,
+		set,
+	}
+
+	struct VarState
+	{
+		mixin(bitfields!(
+			uint, "state", 2,
+			bool, "sign", 1,
+			uint, "eq", 29
+			));
+	}
 
 	void writeAssignment()
 	{
 		writef("v");
-		for(int i = 0; i < varCount; ++i)
-			if(assign[2*i])
-				writef(" %s", i+1);
-			else
-				writef(" -%s", i+1);
+		for(uint v = 0; v < varCount; ++v)
+		{
+			assert(var[v].state == set);
+			writef(" %s", Lit(v, var[v].sign).toDimacs);
+		}
 		writefln(" 0");
 	}
 
-	void setVariable(int x)
+	void setLiteral(Lit l)
 	{
-		if(assign[x^1])
-			throw new Unsat;
-		if(assign[x])
-			return;
-		assign[x] = true;
-
-		auto occsPos = move(occs[x]);
-		auto occsNeg = move(occs[x^1]);
-
-		foreach(i; occsPos)
-			removeClause(i);
-
-		foreach(i; occsNeg)
+		if(var[l.var].state == set)
 		{
-			clauses[i].remove(x^1);
-			if(clauses[i].length == 1)
-				setVariable(clauses[i][0]);
+			if(var[l.var].sign == l.sign)
+				return;
+			else
+				throw new Unsat;
+		}
+
+		var[l.var].state = set;
+		var[l.var].sign = l.sign;
+		prop.push(l.var);
+	}
+
+	void propagate()
+	{
+		while(!prop.empty)
+		{
+			uint v = prop.pop();
+			assert(var[v].state == set);
+			bool s = var[v].sign;
+
+			auto occsPos = move(occs[Lit(v,s)]);
+			auto occsNeg = move(occs[Lit(v,!s)]);
+
+			foreach(i; occsPos)
+				removeClause(i);
+
+			foreach(i; occsNeg)
+			{
+				clauses[i].remove(Lit(v,!s));
+				if(clauses[i].length == 1)
+					setLiteral(clauses[i][0]);
+			}
 		}
 	}
 
 	/** returns false if the clause is trivial, satisfied or unit */
-	bool normalizeClause(ref FlatSet!int c)
+	bool normalizeClause(ref Clause c)
 	{
-		foreach(x, ref bool rem; &c.prune)
-		{
-			if(assign[x])
-				return false;
-			if(assign[x^1])
-				rem = true;
-		}
+		foreach(l, ref bool rem; &c.prune)
+			if(var[l.var].state == set)
+				if(var[l.var].sign == l.sign)
+					return false;
+				else
+					rem = true;
 
 		if(c.length == 0)
 			throw new Unsat;
 
 		if(c.length == 1)
 		{
-			setVariable(c[0]);
+			setLiteral(c[0]);
 			return false;
 		}
 
 		for(size_t i = 1; i < c.length; ++i)
-			if(c[i] == (c[i-1]^1))
+			if(c[i] == c[i-1].neg)
 				return false;
 
 		return true;
 	}
 
-	void addClause(FlatSet!int c)
+	void addClause(Clause c)
 	{
 		if(!normalizeClause(c))
 			return;
@@ -124,10 +201,10 @@ class Sat
 		writefln("c reading file %s: v=%s c=%s", name[max(0,cast(int)$-50)..$], varCount, clauseCount);
 
 		clauses.reserve(clauseCount);
-		assign.resize(2*varCount);
+		var.resize(varCount);
 		occs.resize(2*varCount);
 
-		Array!int c;
+		Array!Lit c;
 
 		for(int i = 0; i < clauseCount; ++i)
 		{
@@ -138,11 +215,10 @@ class Sat
 					throw new Exception("read error");
 				if(x==0) break;
 				assert(-varCount<=x && x <= varCount);
-				x = x>0 ? 2*x-2 : -2*x-1;
-				c.pushBack(x);
+				c.pushBack(Lit.fromDimacs(x));
 			}
 
-			addClause(FlatSet!int(c[]));
+			addClause(Clause(c[]));
 			c.resize(0);
 		}
 	}
@@ -152,23 +228,19 @@ class Sat
 		foreach(ref c; clauses)
 		{
 			foreach(x; c)
-				writef("%s ", x&1?-((x>>1)+1):((x>>1)+1));
+				writef("%s ", x.toDimacs);
 			writefln("0");
 		}
 	}
 
 	void solve()
 	{
-		auto renum = new int[2*varCount];
+		auto renum = new int[varCount];
 		renum[] = -1;
 		int j = 0;
-		for(int x = 0; x < 2*varCount; x+=2)
-			if(!assign[x] && !assign[x^1])
-			{
-				renum[x] = j;
-				renum[x^1] = j^1;
-				j += 2;
-			}
+		for(int v = 0; v < varCount; ++v)
+			if(var[v].state == undef)
+				renum[v] = j++;
 
 		if(j == 0)
 		{
@@ -176,13 +248,13 @@ class Sat
 			return;
 		}
 
-		auto db = new ClauseDB(j/2);
+		auto db = new ClauseDB(j);
 		foreach(ci, ref c; clauses)
 			if(c.length)
 		{
 			int buf[500];
 			foreach(size_t i, x; c)
-				buf[i] = renum[x];
+				buf[i] = Lit(renum[x.var], x.sign);
 			db.addClause(buf[0..c.length]);
 		}
 
@@ -190,11 +262,16 @@ class Sat
 		if(sol is null)
 			throw new Unsat;
 
-		for(int x = 0; x < 2*varCount; x+=2)
-		if(renum[x] != -1)
+		for(int v = 0; v < varCount; ++v)
+		if(renum[v] != -1)
 		{
-			assign[x] = sol[renum[x]];
-			assign[x^1] = sol[renum[x]^1];
+			assert(var[v].state == undef);
+			var[v].state = set;
+			if(sol[Lit(renum[v], false)])
+				var[v].sign = false;
+			else if(sol[Lit(renum[v], true)])
+				var[v].sign = true;
+			else assert(false);
 		}
 	}
 
@@ -204,6 +281,7 @@ class Sat
 		try
 		{
 			sat.readFile();
+			sat.propagate();
 			sat.solve();
 			writefln("s SATISFIABLE");
 			sat.writeAssignment();
