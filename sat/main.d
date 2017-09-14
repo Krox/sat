36c63a46e758c168b3f6c97b5814c09d4f6d17a8
Simon Bürger <simon.buerger@rwth-aachen.de>
2017-09-14 18:02:14 +0200

    integrate solution checker into main application

--- sat/main.d
+++ sat/main.d
@@ -15,19 +15,43 @@ import sat.sat, sat.parser, sat.solver;
  */
 int main(string[] args)
 {
+	bool doCheck = false;
+
 	auto helpInfo = getopt(args,
 		"binary-subsume", "Binary self-subsuming resolution on top-level.", &config.binarySubsume,
 		"xor", "Gaussian elimination of xor clauses on top-level.", &config.xor,
 		"otf", "Simplification of learnt clauses. (0=none, 1=basic, 2=full)", &config.otf,
 		"hyperBinary", "Lazy hyper-binary resolution.", &config.hyperBinary,
 		"watch-stats", "Keep and print statistics on watch-lists lengths.", &config.watchStats,
+		"c", "don't solve, just check solution", &doCheck,
 		);
 
 	// if -h was passed, print help text and quit
-	if(helpInfo.helpWanted || (args.length != 2 && args.length != 3))
+	if(helpInfo.helpWanted || (args.length != 2 && args.length != 3) || (doCheck && args.length != 3))
 	{
-		defaultGetoptPrinter("usage: sat <cnf input> [solution output]", helpInfo.options);
+		defaultGetoptPrinter("usage: sat <cnf input> [solution output]\n       sat -c <cnf input> <solution input>", helpInfo.options);
 		return 0;
+	}
+
+	// if check was requested, do so and quit
+	if(doCheck)
+	{
+		writefln("c reading cnf file: %s", args[1]);
+		auto sat = readDimacs(args[1]);
+
+		writefln("c reading solution file: %s", args[2]);
+		auto sol = readSolution(args[2], sat.varCount);
+
+		if(sat.checkSolution(sol))
+		{
+			writefln("c solution checked");
+			return 0;
+		}
+		else
+		{
+			writefln("c INVALID SOLUTION");
+			return -1;
+		}
 	}
 
 	writefln("c cnf file: %s", args[1]);
