a2e6e27c0a67c4f2f105302d18c529aafd4c17fc
Simon Bürger <simon.buerger@rwth-aachen.de>
2017-10-16 16:27:32 +0200

    integrate other fuzzers from cnf-utils

--- fuzz.py
+++ fuzz.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 from subprocess import call
+from sys import argv
 
 for i in range(0, 1000):
 	print("fuzzing seed", i)
 	cnfFile = open('tmp.cnf', 'w')
 	logFile = open('tmp.log', 'w')
 
-	#r = call(['../cnf-utils/cnf-fuzz-biere', str(i)], stdout=cnfFile)
-	r = call(['../cnf-utils/cnf-fuzz-brummayer.py', '-s', str(i)], stdout=cnfFile)
+	if len(argv) >= 2: mode = int(argv[1])
+	else: mode = 1
+
+	if mode == 1:    cmd = ['../cnf-utils/cnf-fuzz-biere', str(i)]
+	elif mode == 2: cmd = ['../cnf-utils/cnf-fuzz-brummayer.py', '-s', str(i)]
+	elif mode == 3: cmd = ['../cnf-utils/cnf-fuzz-xor.py', '--seed='+str(i)]
+	elif mode == 4: cmd = ['../cnf-utils/largefuzzer', str(i)]
+	elif mode == 5: cmd = ['../cnf-utils/sgen4', '-n', '150', '-sat', '-s', str(i)]
+	elif mode == 6: cmd = ['../cnf-utils/sgen4', '-n', '150', '-unsat', '-s', str(i)]
+	else: raise "invalid mode"
+
+	r = call(cmd, stdout=cnfFile)
 	if r != 0: raise "fuzzer failed"
 	cnfFile.close()
 
